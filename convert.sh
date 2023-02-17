df7c0dd73db62b91d64fdebc1f2079d634d523c2
StableAgOH <stagoh17@gmail.com>
2023-02-17 09:27:57 +0800

    First commit

---
+++ convert.sh
@@ -0,0 +1,14 @@
+rm -rf pdf
+mkdir -p pdf
+mdfiles="md/*.md"
+for mdfile in $mdfiles; do
+    base=$(basename $mdfile .md)
+    echo "Converting $base..."
+    pandoc -s $mdfile -o pdf/$base.pdf --pdf-engine=xelatex --include-in-header=pagenumber.tex \
+        -V CJKmainfont="Smiley Sans" \
+        -V geometry:margin=2cm \
+        -V title=$base
+done
+sleep 1
+echo "Merging PDFs..."
+qpdf --empty --pages pdf/*.pdf -- output.pdf
